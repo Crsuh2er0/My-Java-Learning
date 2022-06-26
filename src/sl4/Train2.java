0efa429085e0a10b97ec9f78e436f845962fefea
bili2er0 <2154818001@qq.com>
2022-06-26 22:14:44 +0800

    220626

---
+++ src/sl4/Train2.java
@@ -0,0 +1,35 @@
+package sl4;
+
+public class Train2 {
+    public static void main(String args[]){
+        int month = 6;
+        String season;
+        switch (month){
+            case 1:
+                season = "Winter";
+            case 2:
+                season = "Spring";
+            case 3:
+                season = "Spring";
+            case 4:
+                season = "Spring";
+            case 5:
+                season = "Summer";
+            case 6:
+                season = "Summer";
+            case 7:
+                season = "Summer";
+            case 8:
+                season = "Autumn";
+            case 9:
+                season = "Autumn";
+            case 10:
+                season = "Autumn";
+            case 11:
+                season = "Winter";
+            case 12:
+                season = "Winter";
+
+        }
+    }
+}
