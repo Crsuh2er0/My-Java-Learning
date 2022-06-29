8b11c657894a58dfaeafbf2f71cd0c8befe7ab77
bili2er0 <2154818001@qq.com>
2022-06-29 18:12:23 +0800

    220629

---
+++ src/sl4/Train3.java
@@ -0,0 +1,11 @@
+package sl4;
+
+public class Train3 {
+    public static void main(String args[]){
+        int num = 1;
+        for(int t=1;t<=10;t++){
+            num *= 2;
+        }
+        System.out.println("第十代菌落种的细菌数量为：" + num);
+    }
+}
---
+++ src/sl4/Train4.java
@@ -0,0 +1,17 @@
+package sl4;
+
+public class Train4 {
+    public static void main(String args[]){
+        int a = 1;
+        int b = 1;
+        int n = 53;
+        int tem,num;
+        for (int t=1;t<n - 1;t++){
+            tem = a + b;
+            a = b;
+            b = tem;
+        }
+        num = b;
+        System.out.println("这组数中的第n个数值为：" + num);
+    }
+}
